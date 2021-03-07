276fc27976c1544efca13d8ae3e615030fb1543d
AHipWhale <charlie.choffat@gmail.com>
2021-03-07 18:33:15 +0100

    neuronlayer en neuronnetwork toegevoegd

---
+++ P3/code/neuronlayer.py
@@ -0,0 +1,19 @@
+class NeuronLayer:
+    def __init__(self, neuron):
+        self.neuron = neuron
+        self.input = []
+        self.output = []
+
+    def outputFunctie(self, input: list):
+        """Deze functie geeft de outputs van alle neuronen in de layer"""
+        outputs = []
+        self.input = input
+        for neuron in self.neuron:
+            outputs.append(neuron.output(input))
+        self.output = outputs
+        return outputs
+
+    def __str__(self):
+        """Deze functie returnt de belangrijke informatie van de neuron"""
+        return "De lengte van deze layer is %s neuronen en de output van deze neuronen is %s" % \
+               (len(self.neuron), self.outputFunctie(self.input))
