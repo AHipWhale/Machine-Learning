6a6483b34c9b1ce869d097f8fe21591a0cddce67
AHipWhale <charlie.choffat@gmail.com>
2021-03-07 19:00:50 +0100

    feedback van neuron.py verbeterd

--- P3/code/neuron.py
+++ P3/code/neuron.py
@@ -4,10 +4,9 @@ class Neuron:
     def __init__(self, weights, bias):
         self.bias = bias
         self.weights = weights
-        self.outputs = []
         self.sumInvoer = None
 
-    def activation_function(self, invoer: [float]):
+    def calculate_input(self, invoer: [float]):
         """Deze functie berekent de som van de inputs met we weights en telt daarbij de bias op"""
         self.sumInvoer = 0
         for index in range(len(self.weights)):
@@ -15,11 +14,11 @@ class Neuron:
         self.sumInvoer += self.bias
         return self.sumInvoer
 
-    def output(self):
-        """Deze functie kijkt of de som van de inputs*weights+bias of lager is dan de treshold en returnt dan de output"""
-        return 1/(1+math.exp(-self.sumInvoer))
+    def activation_function(self, invoer: [float]):
+        """Deze functie berekent de sigmoid en returnt dat de output"""
+        return 1/(1+math.exp(-self.calculate_input(invoer)))
 
     def __str__(self):
         """Deze functie returnt de belangrijke informatie van de neuron"""
         return "De weights waren %s en een bias van %s. De output van de neuron was %s" % \
-               (self.weights, self.bias, self.outputs)
+               (self.weights, self.bias, self.sumInvoer)
